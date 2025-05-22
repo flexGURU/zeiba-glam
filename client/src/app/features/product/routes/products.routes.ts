8d817910ee8df7a8fb946ba638a5d610f3ad1345
flexGURU <mukunajohn329@gmail.com>
2025-05-22 05:56:09 +0300

    changed directory

--- client/src/app/features/product/routes/products.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { ProductDetailComponent } from '../product-list/product-detail/product-detail.component';
-
-export const routes: Routes = [
-  {
-    path: '',
-    component: ProductDetailComponent,
-  },
-];
